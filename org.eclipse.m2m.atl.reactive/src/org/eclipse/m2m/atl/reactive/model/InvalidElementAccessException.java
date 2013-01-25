5a9a662451db7ae2001da79380fd955a11cb9318
mtisi <mtisi@1201807c-809d-0410-8234-cbf3d9c0f928>
2013-01-25 10:09:29 +0000

    removed todo
    git-svn-id: https://campus.mines-nantes.fr/atlanmod/svn/LazyATLPrototype/trunk@228 1201807c-809d-0410-8234-cbf3d9c0f928

--- org.eclipse.m2m.atl.reactive/src/org/eclipse/m2m/atl/reactive/model/InvalidElementAccessException.java
+++ org.eclipse.m2m.atl.reactive/src/org/eclipse/m2m/atl/reactive/model/InvalidElementAccessException.java
@@ -2,16 +2,11 @@ package org.eclipse.m2m.atl.reactive.model;
 
 public class InvalidElementAccessException extends LazyModelRuntimeException{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public InvalidElementAccessException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
-	}
 
-	
+	}
 
 }
